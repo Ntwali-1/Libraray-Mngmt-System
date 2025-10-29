18362e215cfd65ad87356b145197c7e96c622b2e
Ntwali-1 <ntwaliyanis@gmail.com>
2025-10-29 20:44:23 +0200

    first commit

---
+++ src/Book.java
@@ -0,0 +1,17 @@
+public class Book {
+    private String title;
+    private String author;
+    private String isAvailable;
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public void setIsAvailable(String isAvailable) {
+        this.isAvailable = isAvailable;
+    }
+}
