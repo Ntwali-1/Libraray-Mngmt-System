d8ac2fdd779fdcc2774736bff76ec55015c6c87d
Ntwali-1 <ntwaliyanis@gmail.com>
2025-10-29 20:59:59 +0200

    refactor: Student

--- src/Student.java
+++ src/Student.java
@@ -6,8 +6,15 @@ public class Student extends Person {
         this.department = department;
     }
 
+    public String getDepartment() {
+        return department;
+    }
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
     public void borrowBook (Book b){
-        if(b.getIsAvailable() == true){
+        if(b.getIsAvailable()){
             System.out.println("Book is available and borrowed");
         }else {
             System.out.println("Book is not available");
